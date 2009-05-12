f62ee8203046deda902f2e7a4fb18a8debd3e7df
Paul Battley <pbattley@gmail.com>
2009-05-12 15:11:20 +0100

    Don't ask me, just do it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 # Bootstrap chef
 
 apt-get update
-apt-get install rdoc ruby-dev
+apt-get install -y rdoc ruby-dev
 cd /tmp
 curl -L 'http://rubyforge.org/frs/download.php/56227/rubygems-1.3.3.tgz' | tar zxv
 cd rubygems-1.3.3
