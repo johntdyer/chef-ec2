65abf80f9dafade446e0f78bac03056d10ab2de4
Paul Battley <pbattley@gmail.com>
2009-05-12 16:40:36 +0100

    The cookbooks expect gem to be called gem. It's easier if we conform.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ cd /tmp
 curl -L 'http://rubyforge.org/frs/download.php/56227/rubygems-1.3.3.tgz' | tar zxv
 cd rubygems-1.3.3
 ruby setup.rb
+ln -s /usr/bin/gem1.8 /usr/bin/gem
 cd
 gem1.8 install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org
 mkdir -p /etc/chef
