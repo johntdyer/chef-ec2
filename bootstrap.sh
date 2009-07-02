f5afd07a7e461b92d8ba455ef9833edd1b73c092
Paul Paradise <paul.paradise@socrata.com>
2009-07-02 13:48:13 -0700

    RubyGems 1.3.4, fixed ruby-dev package, don't install rdoc

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,12 @@
 # Bootstrap chef
 
 apt-get update
-apt-get install -y rdoc ruby-dev
+apt-get install -y rdoc ruby1.8-dev
 cd /tmp
-curl -L 'http://rubyforge.org/frs/download.php/56227/rubygems-1.3.3.tgz' | tar zxv
-cd rubygems-1.3.3
+curl -L 'http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz' | tar xvz
+cd rubygems-1.3.4
 ruby setup.rb
-ln -s /usr/bin/gem1.8 /usr/bin/gem
+ln -s gem1.8 /usr/bin/gem
 cd
-gem1.8 install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org
+gem install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org --no-rdoc --no-ri
 mkdir -p /etc/chef
