4fc0428bea1e3074f7ff3b4c9af275ba35aa5144
Paul Paradise <paul.paradise@socrata.com>
2009-07-07 17:56:45 -0700

    Don't install RDoc/RI for RubyGems; auto-install build-essential bits

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,11 +3,11 @@
 # Bootstrap chef
 
 apt-get update
-apt-get install -y rdoc ruby1.8-dev
+apt-get install -y rdoc ruby1.8-dev build-essential
 cd /tmp
 curl -L 'http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz' | tar xvz
 cd rubygems-1.3.4
-ruby setup.rb
+ruby setup.rb --no-rdoc --no-ri
 ln -s gem1.8 /usr/bin/gem
 cd
 gem install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org --no-rdoc --no-ri
