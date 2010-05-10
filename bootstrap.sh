3cdd250cfc01e704e999a67672622d6576a864b8
Paul Battley <pbattley@gmail.com>
2010-05-10 17:51:44 +0100

    Updated RubyGems to 1.3.6 in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,16 @@
 
 # Bootstrap chef
 
-RUBYGEM_VERSION="1.3.5"
+RUBYGEM_VERSION="1.3.6"
 
 apt-get update
 apt-get install -y rdoc ruby1.8-dev build-essential
 cd /tmp
-curl -L "http://rubyforge.org/frs/download.php/57643/rubygems-${RUBYGEM_VERSION}.tgz" | tar xvz
+curl -L "http://production.cf.rubygems.org/rubygems/rubygems-${RUBYGEM_VERSION}.tgz" | tar xvz
 cd rubygems-${RUBYGEM_VERSION}
 ruby setup.rb --no-rdoc --no-ri
-ln -s gem1.8 /usr/bin/gem
+ln -s `which gem1.8` /usr/bin/gem
 cd
 gem update --system
-gem install chef ohai --source http://gems.opscode.com --source http://gems.rubyforge.org --no-rdoc --no-ri
+gem install chef ohai --no-rdoc --no-ri
 mkdir -p /etc/chef
