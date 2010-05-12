57d660df19455e835bab2d56db90150497e2952c
Paul Battley <pbattley@gmail.com>
2010-05-12 12:30:08 +0100

    Install REE straight off rather than compiling RubyGems.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,22 @@
 
 # Bootstrap chef
 
-RUBYGEM_VERSION="1.3.6"
-
 apt-get update
-apt-get install -y rdoc ruby1.8-dev build-essential
+apt-get install -y build-essential
+
+case `uname -m` in
+  x86_64)
+    REE_DEB="ruby-enterprise_1.8.7-2010.01_amd64.deb"
+    ;;
+  *)
+    REE_DEB="ruby-enterprise_1.8.7-2010.01_i386.deb"
+    ;;
+esac
+
 cd /tmp
-curl -L "http://production.cf.rubygems.org/rubygems/rubygems-${RUBYGEM_VERSION}.tgz" | tar xvz
-cd rubygems-${RUBYGEM_VERSION}
-ruby setup.rb --no-rdoc --no-ri
-ln -s `which gem1.8` /usr/bin/gem
-cd
+curl -O -L "http://rubyforge.org/frs/download.php/68718/${REE_DEB}"
+dpkg -i ${REE_DEB}
+
 gem update --system
 gem install chef ohai --no-rdoc --no-ri
 mkdir -p /etc/chef
