5c51d4407cc0f2f3a09f1c368278abffb1029630
Paul Battley <pbattley@gmail.com>
2010-06-03 11:07:53 +0100

    Make bootstrapping idempotent, and use known-good versions of gems and chef.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,22 +2,32 @@
 
 # Bootstrap chef
 
-apt-get update
+command -v chef-solo >/dev/null && {
+  echo "Chef is already bootstrapped"
+  exit
+}
+
+apt-get update && \
 apt-get install -y build-essential
 
 case `uname -m` in
   x86_64)
-    REE_DEB="ruby-enterprise_1.8.7-2010.01_amd64.deb"
+    REE="http://rubyforge.org/frs/download.php/68720/ruby-enterprise_1.8.7-2010.01_amd64.deb"
     ;;
   *)
-    REE_DEB="ruby-enterprise_1.8.7-2010.01_i386.deb"
+    REE="http://rubyforge.org/frs/download.php/68718/ruby-enterprise_1.8.7-2010.01_i386.deb"
     ;;
 esac
 
-cd /tmp
-curl -O -L "http://rubyforge.org/frs/download.php/68718/${REE_DEB}"
-dpkg -i ${REE_DEB}
+echo "Fetching ${REE}"
+curl -s -L -o ree.deb "${REE}" && \
+dpkg -i ree.deb && \
+rm ree.deb
 
-gem update --system
-gem install chef ohai --no-rdoc --no-ri
+RUBYGEMS_VERSION="1.3.6"
+gem install -v ${RUBYGEMS_VERSION} rubygems-update && \
+ruby `gem env gemdir`/gems/rubygems-update-${RUBYGEMS_VERSION}/setup.rb && \
+gem install -v 0.5.4  ohai --no-rdoc --no-ri
+gem install -v 0.8.16 chef --no-rdoc --no-ri
+
 mkdir -p /etc/chef
